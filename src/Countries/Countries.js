7ba57e8ec51f66adeeff03f65eecf0bb5451d6d5
Aziz Ali <aziz@ilovecoding.org>
2019-02-15 17:12:10 -0600

    Countries field setup

--- src/Countries/Countries.js
+++ src/Countries/Countries.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
-export default function({ changeCb, selected }) {
-	return (
-		<div className="col">
-			<label htmlFor="country">Select Country</label>
-			<select className="custom-select" id="country">
-				<option value="" />
-			</select>
-		</div>
-	);
+export default class Countries extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			countries: [ 'A', 'B' ]
+		};
+
+		this.changeCountry = this.changeCountry.bind(this);
+	}
+	changeCountry(e) {
+		this.setState({
+			selected: e.target.value
+		});
+	}
+	render() {
+		const { countries, selected } = this.state;
+		return (
+			<div className="col">
+				<label htmlFor="country">Select Country</label>
+				<select value={selected} onChange={this.changeCountry} className="custom-select" id="country">
+					{countries.map((item) => {
+						return (
+							<option key={item} value={item}>
+								{item}
+							</option>
+						);
+					})}
+				</select>
+			</div>
+		);
+	}
 }
